314b5d6a8d00f01581bf8dadb19eed512b5470c8
Bhavishya Dahiya <ghost25112006@gmail.com>
2026-02-01 10:14:02 +0530

    Update internetIncome.sh

--- internetIncome.sh
+++ internetIncome.sh
@@ -537,7 +537,7 @@ start_containers() {
     if [ "$container_pulled" = false ]; then
       sudo docker pull techroy23/docker-packetsdk:latest
     fi
-    docker_parameters=($LOGS_PARAM $DNS_VOLUME $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $NETWORK_TUN techroy23/docker-packetsdk:latest -appkey=$PACKET_SDK_APP_KEY)
+    docker_parameters=($LOGS_PARAM $DNS_VOLUME $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $NETWORK_TUN techroy23/docker-packetsdk:latest -e APPKEY=$PACKET_SDK_APP_KEY)
     execute_docker_command "PacketSDK" "packetsdk$UNIQUE_ID$i" "${docker_parameters[@]}"
   else
     if [[ "$container_pulled" == false && "$ENABLE_LOGS" == true ]]; then
